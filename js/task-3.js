5deacac1572cb59dee2b473d6da5c20ad72d938b
AllaPavuk <Pavuchok24@gmail.com>
2026-04-18 17:42:53 +0300

    first add

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,19 @@
-function getElementWidth(content, padding, border) {
-    const boxSizing = parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-    return boxSizing;
+function checkForSpam(message) {
+    const lowMessage = message.toLowerCase();
+    const bulWord1 = "spam";
+    const bulWord2 = "sale";
+
+    const hasWord1 = lowMessage.includes("spam");
+    const hasWord2 = lowMessage.includes("sale");
+    const hasbulWords = hasWord1 || hasWord2;
+
+    return hasbulWords;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
